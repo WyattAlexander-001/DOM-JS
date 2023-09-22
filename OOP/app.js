12bb0a267d769ebb2823dc0c96dcc71ee1bd1a52
WyattAlexander-001 <wyattbushman117@gmail.com>
2023-09-22 00:10:08 -0400

    I don't like this non-java object

--- OOP/app.js
+++ OOP/app.js
@@ -6,6 +6,7 @@ function rgb(r,g,b){
     return `rgb(${r},${g},${b})`;
 }
 
+//This is ugly pattern
 function makeColor(r,g,b){
     const color = {};
     color.r = r;
@@ -13,10 +14,17 @@ function makeColor(r,g,b){
     color.b = b;
 
     color.rgb = function(){
-        
+        const {r,g,b} = this;
+        return `rgb(${r},${g},${b})`;
+    };
+    color.hex = function(){
+        const {r,g,b} = this;
+        return '#' + ((1<<24) + (r<<16) + (g<<8) +b).toString(16).slice(1);
     }
     return color;
 }
+    
+
 
 
 
@@ -26,3 +34,5 @@ function makeColor(r,g,b){
 //hex(200,666,25); //'#ca9a19'
 console.log(hex(0,22,3)); //#001603
 hex(0,22,3); //Won't print to console since it's only returned
+
+const testColor = makeColor(35,255,150);
